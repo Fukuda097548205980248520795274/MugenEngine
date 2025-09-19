7f10310005c3243c8e392e71dc8027b92ab32c03
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2025-09-20 07:52:25 +0900

    3x3行列を作成した。

--- Engine/Include/Math/Matrix3x3/Matrix3x3.cpp
+++ Engine/Include/Math/Matrix3x3/Matrix3x3.cpp
@@ -1,1 +1,296 @@
 #include "Matrix3x3.h"
+
+/// <summary>
+/// 座標変換を行う
+/// </summary>
+/// <param name="vector"></param>
+/// <param name="matrix"></param>
+/// <returns></returns>
+Vector2 Transform(const Vector2& vector, const Matrix3x3& matrix)
+{
+	// 座標変換した値
+	Vector2 transform;
+
+	transform.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[0][1] + 1.0f * matrix.m[0][2];
+	transform.y = vector.x * matrix.m[1][0] + vector.y * matrix.m[1][1] + 1.0f * matrix.m[1][2];
+	float w = vector.x * matrix.m[2][0] + vector.y * matrix.m[2][1] + 1.0f * matrix.m[2][2];
+
+	assert(w != 0.0f);
+	transform /= w;
+
+	return transform;
+}
+
+/// <summary>
+/// 座標変換を行う
+/// </summary>
+/// <param name="vector"></param>
+/// <param name="matrix"></param>
+/// <returns></returns>
+Vector3 Transform(const Vector3& vector, const Matrix3x3& matrix)
+{
+	// 座標変換した値
+	Vector3 transform;
+
+	transform.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[0][1] + vector.z * matrix.m[0][2];
+	transform.y = vector.x * matrix.m[1][0] + vector.y * matrix.m[1][1] + vector.z * matrix.m[1][2];
+	transform.z = vector.x * matrix.m[2][0] + vector.y * matrix.m[2][1] + vector.z * matrix.m[2][2];
+
+	assert(transform.z != 0.0f);
+	transform.x /= transform.z;
+	transform.y /= transform.z;
+	transform.z /= transform.z;
+
+	return transform;
+}
+
+/// <summary>
+/// 座標変換を行う
+/// </summary>
+/// <param name="vector"></param>
+/// <param name="matrix"></param>
+/// <returns></returns>
+Vector2 TransformNormal(const Vector2& vector, const Matrix3x3& matrix)
+{
+	// 座標変換した値
+	Vector2 transform;
+
+	transform.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[0][1];
+	transform.y = vector.x * matrix.m[1][0] + vector.y * matrix.m[1][1];
+
+	return transform;
+}
+
+/// <summary>
+/// 単位行列を作成する
+/// </summary>
+/// <returns></returns>
+Matrix3x3 MakeIdentityMatrix()
+{
+	// 単位行列
+	Matrix3x3 identityMatrix;
+
+	for (uint32_t i = 0; i < 3; ++i)
+	{
+		for (uint32_t j = 0; j < 3; ++j)
+		{
+			if (i == j)
+			{
+				identityMatrix.m[i][j] = 1.0f;
+			} else
+			{
+				identityMatrix.m[i][j] = 0.0f;
+			}
+		}
+	}
+
+	return identityMatrix;
+}
+
+/// <summary>
+/// 転置行列を作成する
+/// </summary>
+/// <param name="matrix"></param>
+/// <returns></returns>
+Matrix3x3 MakeTransposeMatrix(const Matrix3x3& matrix)
+{
+	// 転置行列
+	Matrix3x3 transposeMatrix;
+
+	transposeMatrix.m[0][0] = matrix.m[0][0];
+	transposeMatrix.m[0][1] = matrix.m[1][0];
+	transposeMatrix.m[0][2] = matrix.m[2][0];
+
+	transposeMatrix.m[1][0] = matrix.m[0][1];
+	transposeMatrix.m[1][1] = matrix.m[1][1];
+	transposeMatrix.m[1][2] = matrix.m[2][1];
+
+	transposeMatrix.m[2][0] = matrix.m[0][2];
+	transposeMatrix.m[2][1] = matrix.m[1][2];
+	transposeMatrix.m[2][2] = matrix.m[2][2];
+
+	return transposeMatrix;
+}
+
+/// <summary>
+/// 拡大縮小行列を作成する
+/// </summary>
+/// <param name="scale"></param>
+/// <returns></returns>
+Matrix3x3 MakeScaleMatrix(const Vector2& scale)
+{
+	// 拡大縮小行列
+	Matrix3x3 scaleMatrix;
+
+	scaleMatrix.m[0][0] = scale.x;
+	scaleMatrix.m[0][1] = 0.0f;
+	scaleMatrix.m[0][2] = 0.0f;
+
+	scaleMatrix.m[1][0] = 0.0f;
+	scaleMatrix.m[1][1] = scale.y;
+	scaleMatrix.m[1][2] = 0.0f;
+
+	scaleMatrix.m[2][0] = 0.0f;
+	scaleMatrix.m[2][1] = 0.0f;
+	scaleMatrix.m[2][2] = 1.0f;
+
+	return scaleMatrix;
+}
+
+/// <summary>
+/// 回転行列を作成する
+/// </summary>
+/// <param name="radian"></param>
+/// <returns></returns>
+Matrix3x3 MakeRotateMatrix(float radian)
+{
+	// 回転行列
+	Matrix3x3 rotateMatrix;
+
+	rotateMatrix.m[0][0] = std::cos(radian);
+	rotateMatrix.m[0][1] = std::sin(radian);
+	rotateMatrix.m[0][2] = 0.0f;
+
+	rotateMatrix.m[1][0] = -std::sin(radian);
+	rotateMatrix.m[1][1] = std::cos(radian);
+	rotateMatrix.m[1][2] = 0.0f;
+
+	rotateMatrix.m[2][0] = 0.0f;
+	rotateMatrix.m[2][1] = 0.0f;
+	rotateMatrix.m[2][2] = 1.0f;
+
+	return rotateMatrix;
+}
+
+/// <summary>
+/// 平行移動行列を作成する
+/// </summary>
+/// <param name="translation"></param>
+/// <returns></returns>
+Matrix3x3 MakeTranslateMatrix(const Vector2& translation)
+{
+	// 平行移動行列
+	Matrix3x3 translateMatrix;
+
+	translateMatrix.m[0][0] = 1.0f;
+	translateMatrix.m[0][1] = 0.0f;
+	translateMatrix.m[0][2] = 0.0f;
+
+	translateMatrix.m[1][0] = 0.0f;
+	translateMatrix.m[1][1] = 1.0f;
+	translateMatrix.m[1][2] = 0.0f;
+	
+	translateMatrix.m[2][0] = translation.x;
+	translateMatrix.m[2][1] = translation.y;
+	translateMatrix.m[2][2] = 1.0f;
+
+	return translateMatrix;
+}
+
+/// <summary>
+/// アフィン変換行列を作成する
+/// </summary>
+/// <param name="scale"></param>
+/// <param name="radian"></param>
+/// <param name="translation"></param>
+/// <returns></returns>
+Matrix3x3 MakeAffineMatrix(const Vector2& scale, float radian, const Vector2& translation)
+{
+	// アフィン変換行列
+	Matrix3x3 affineMatrix = MakeScaleMatrix(scale) * MakeRotateMatrix(radian) * MakeTranslateMatrix(translation);
+	return affineMatrix;
+}
+
+/// <summary>
+/// 逆行列を作成する
+/// </summary>
+/// <param name="m"></param>
+/// <returns></returns>
+Matrix3x3 MakeInverseMatrix(const Matrix3x3& m)
+{
+	// 行列式
+	float determinant =
+		m.m[0][0] * m.m[1][1] * m.m[2][2] + m.m[0][1] * m.m[1][2] * m.m[2][0] + m.m[0][2] * m.m[1][0] * m.m[2][1] -
+		m.m[0][2] * m.m[1][1] * m.m[2][0] - m.m[0][1] * m.m[1][0] * m.m[2][2] - m.m[0][0] * m.m[1][2] * m.m[2][1];
+
+	// 余因子行列
+	Matrix3x3 adjugateMatrix;
+
+	adjugateMatrix.m[0][0] = m.m[1][1] * m.m[2][2] - m.m[1][2] * m.m[2][1];
+	adjugateMatrix.m[0][1] = -(m.m[0][1] * m.m[2][2] - m.m[0][2] * m.m[2][1]);
+	adjugateMatrix.m[0][2] = m.m[0][1] * m.m[1][2] - m.m[0][2] * m.m[1][1];
+
+	adjugateMatrix.m[1][0] = -(m.m[1][0] * m.m[2][2] - m.m[1][2] * m.m[2][0]);
+	adjugateMatrix.m[1][1] = m.m[0][0] * m.m[2][2] - m.m[0][2] * m.m[2][0];
+	adjugateMatrix.m[1][2] = -(m.m[0][0] * m.m[1][2] - m.m[0][2] * m.m[1][0]);
+
+	adjugateMatrix.m[2][0] = m.m[1][0] * m.m[2][1] - m.m[1][1] * m.m[2][0];
+	adjugateMatrix.m[2][1] = -(m.m[0][0] * m.m[2][1] - m.m[0][1] * m.m[2][0]);
+	adjugateMatrix.m[2][2] = m.m[0][0] * m.m[1][1] - m.m[0][1] * m.m[1][0];
+
+	for (int32_t i = 0; i < 3; ++i)
+	{
+		for (int32_t j = 0; j < 3; ++j)
+		{
+			adjugateMatrix.m[i][j] *= 1.0f / determinant;
+		}
+	}
+
+	return adjugateMatrix;
+}
+
+/// <summary>
+/// 平行投影行列を作成する
+/// </summary>
+/// <param name="left"></param>
+/// <param name="top"></param>
+/// <param name="right"></param>
+/// <param name="bottom"></param>
+/// <returns></returns>
+Matrix3x3 MakeOrthographicMatrix(float left, float top, float right, float bottom)
+{
+	// 平行投影行列
+	Matrix3x3 orthographicMatrix;
+
+	orthographicMatrix.m[0][0] = 2.0f / (right - left);
+	orthographicMatrix.m[0][1] = 0.0f;
+	orthographicMatrix.m[0][2] = 0.0f;
+
+	orthographicMatrix.m[1][0] = 0.0f;
+	orthographicMatrix.m[1][1] = 2.0f / (top - bottom);
+	orthographicMatrix.m[1][2] = 0.0f;
+
+	orthographicMatrix.m[2][0] = (left + right) / (left - right);
+	orthographicMatrix.m[2][1] = (top + bottom) / (bottom - top);
+	orthographicMatrix.m[2][2] = 1.0f;
+
+	return orthographicMatrix;
+}
+
+/// <summary>
+/// ビューポート変換行列を作成する
+/// </summary>
+/// <param name="left"></param>
+/// <param name="top"></param>
+/// <param name="width"></param>
+/// <param name="height"></param>
+/// <returns></returns>
+Matrix3x3 MakeViewportMatrix(float left, float top, float width, float height)
+{
+	// ビューポート変換行列
+	Matrix3x3 viewportMatrix;
+
+	viewportMatrix.m[0][0] = width / 2.0f;
+	viewportMatrix.m[0][1] = 0.0f;
+	viewportMatrix.m[0][2] = 0.0f;
+
+	viewportMatrix.m[1][0] = 0.0f;
+	viewportMatrix.m[1][1] = -(height / 2.0f);
+	viewportMatrix.m[1][2] = 0.0f;
+
+	viewportMatrix.m[2][0] = left + (width / 2.0f);
+	viewportMatrix.m[2][1] = top + (height / 2.0f);
+	viewportMatrix.m[2][2] = 1.0f;
+
+	return viewportMatrix;
+}
--- Engine/Include/Math/Matrix3x3/Matrix3x3.h
+++ Engine/Include/Math/Matrix3x3/Matrix3x3.h
@@ -9,4 +9,257 @@
 struct Matrix3x3
 {
 	float m[3][3];
+
+	// 加算
+	Matrix3x3 operator+=(const Matrix3x3& matrix)
+	{
+		for (int32_t i = 0; i < 3; ++i)
+		{
+			for (int32_t j = 0; j < 3; ++j)
+			{
+				this->m[i][j] += matrix.m[i][j];
+			}
+		}
+
+		return *this;
+	}
+
+	// 減算
+	Matrix3x3 operator-=(const Matrix3x3& matrix)
+	{
+		for (int32_t i = 0; i < 3; ++i)
+		{
+			for (int32_t j = 0; j < 3; ++j)
+			{
+				this->m[i][j] -= matrix.m[i][j];
+			}
+		}
+
+		return *this;
+	}
+
+	// スカラー倍
+	Matrix3x3 operator*=(float scalar)
+	{
+		for (uint32_t i = 0; i < 3; ++i)
+		{
+			for (uint32_t j = 0; j < 3; ++j)
+			{
+				this->m[i][j] *= scalar;
+			}
+		}
+
+		return *this;
+	}
+
+	// 乗算
+	Matrix3x3 operator*=(const Matrix3x3& matrix)
+	{
+		// 積
+		Matrix3x3 multiply;
+
+		multiply.m[0][0] = this->m[0][0] * matrix.m[0][0] + this->m[0][1] * matrix.m[1][0] + this->m[0][2] * matrix.m[2][0];
+		multiply.m[0][1] = this->m[0][0] * matrix.m[0][1] + this->m[0][1] * matrix.m[1][1] + this->m[0][2] * matrix.m[2][1];
+		multiply.m[0][2] = this->m[0][0] * matrix.m[0][2] + this->m[0][1] * matrix.m[1][2] + this->m[0][2] * matrix.m[2][2];
+
+		multiply.m[1][0] = this->m[1][0] * matrix.m[0][0] + this->m[1][1] * matrix.m[1][0] + this->m[1][2] * matrix.m[2][0];
+		multiply.m[1][1] = this->m[1][0] * matrix.m[0][1] + this->m[1][1] * matrix.m[1][1] + this->m[1][2] * matrix.m[2][1];
+		multiply.m[1][2] = this->m[1][0] * matrix.m[0][2] + this->m[1][1] * matrix.m[1][2] + this->m[1][2] * matrix.m[2][2];
+
+		multiply.m[2][0] = this->m[2][0] * matrix.m[0][0] + this->m[2][1] * matrix.m[1][0] + this->m[2][2] * matrix.m[2][0];
+		multiply.m[2][1] = this->m[2][0] * matrix.m[0][1] + this->m[2][1] * matrix.m[1][1] + this->m[2][2] * matrix.m[2][1];
+		multiply.m[2][2] = this->m[2][0] * matrix.m[0][2] + this->m[2][1] * matrix.m[1][2] + this->m[2][2] * matrix.m[2][2];
+
+		*this = multiply;
+
+		return *this;
+	}
 };
+
+
+namespace
+{
+	// 加算
+	Matrix3x3 operator+(const Matrix3x3& m1, const Matrix3x3& m2)
+	{
+		// 加算
+		Matrix3x3 add;
+
+		for (int32_t i = 0; i < 3; ++i)
+		{
+			for (int32_t j = 0; j < 3; ++j)
+			{
+				add.m[i][j] = m1.m[i][j] + m2.m[i][j];
+			}
+		}
+
+		return add;
+	}
+
+	// 減算
+	Matrix3x3 operator-(const Matrix3x3& m1, const Matrix3x3& m2)
+	{
+		// 加算
+		Matrix3x3 subtract;
+
+		for (int32_t i = 0; i < 3; ++i)
+		{
+			for (int32_t j = 0; j < 3; ++j)
+			{
+				subtract.m[i][j] = m1.m[i][j] - m2.m[i][j];
+			}
+		}
+
+		return subtract;
+	}
+
+	// スカラー倍
+	Matrix3x3 operator*(float scalar, const Matrix3x3& matrix)
+	{
+		// スカラー倍
+		Matrix3x3 scalarMultiply;
+
+		for (uint32_t i = 0; i < 3; ++i)
+		{
+			for (uint32_t j = 0; j < 3; ++j)
+			{
+				scalarMultiply.m[i][j] = scalar * matrix.m[i][j];
+			}
+		}
+
+		return scalarMultiply;
+	}
+
+	// スカラー倍
+	Matrix3x3 operator*(const Matrix3x3& matrix, float scalar)
+	{
+		// スカラー倍
+		Matrix3x3 scalarMultiply;
+
+		for (uint32_t i = 0; i < 3; ++i)
+		{
+			for (uint32_t j = 0; j < 3; ++j)
+			{
+				scalarMultiply.m[i][j] = matrix.m[i][j] * scalar;
+			}
+		}
+
+		return scalarMultiply;
+	}
+
+	// 乗算
+	Matrix3x3 operator*(const Matrix3x3& m1, const Matrix3x3& m2)
+	{
+		// 積
+		Matrix3x3 multiply;
+
+		multiply.m[0][0] = m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0] + m1.m[0][2] * m2.m[2][0];
+		multiply.m[0][1] = m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1] + m1.m[0][2] * m2.m[2][1];
+		multiply.m[0][2] = m1.m[0][0] * m2.m[0][2] + m1.m[0][1] * m2.m[1][2] + m1.m[0][2] * m2.m[2][2];
+
+		multiply.m[1][0] = m1.m[1][0] * m2.m[0][0] + m1.m[1][1] * m2.m[1][0] + m1.m[1][2] * m2.m[2][0];
+		multiply.m[1][1] = m1.m[1][0] * m2.m[0][1] + m1.m[1][1] * m2.m[1][1] + m1.m[1][2] * m2.m[2][1];
+		multiply.m[1][2] = m1.m[1][0] * m2.m[0][2] + m1.m[1][1] * m2.m[1][2] + m1.m[1][2] * m2.m[2][2];
+
+		multiply.m[2][0] = m1.m[2][0] * m2.m[0][0] + m1.m[2][1] * m2.m[1][0] + m1.m[2][2] * m2.m[2][0];
+		multiply.m[2][1] = m1.m[2][0] * m2.m[0][1] + m1.m[2][1] * m2.m[1][1] + m1.m[2][2] * m2.m[2][1];
+		multiply.m[2][2] = m1.m[2][0] * m2.m[0][2] + m1.m[2][1] * m2.m[1][2] + m1.m[2][2] * m2.m[2][2];
+
+		return multiply;
+	}
+}
+
+
+/// <summary>
+/// 座標変換を行う
+/// </summary>
+/// <param name="vector"></param>
+/// <param name="matrix"></param>
+/// <returns></returns>
+Vector2 Transform(const Vector2& vector, const Matrix3x3& matrix);
+
+/// <summary>
+/// 座標変換を行う
+/// </summary>
+/// <param name="vector"></param>
+/// <param name="matrix"></param>
+/// <returns></returns>
+Vector3 Transform(const Vector3& vector, const Matrix3x3& matrix);
+
+/// <summary>
+/// 座標変換を行う
+/// </summary>
+/// <param name="vector"></param>
+/// <param name="matrix"></param>
+/// <returns></returns>
+Vector2 TransformNormal(const Vector2& vector, const Matrix3x3& matrix);
+
+/// <summary>
+/// 単位行列を作成する
+/// </summary>
+/// <returns></returns>
+Matrix3x3 MakeIdentityMatrix();
+
+/// <summary>
+/// 転置行列を作成する
+/// </summary>
+/// <param name="matrix"></param>
+/// <returns></returns>
+Matrix3x3 MakeTransposeMatrix(const Matrix3x3& matrix);
+
+/// <summary>
+/// 拡大縮小行列を作成する
+/// </summary>
+/// <param name="scale"></param>
+/// <returns></returns>
+Matrix3x3 MakeScaleMatrix(const Vector2& scale);
+
+/// <summary>
+/// 回転行列を作成する
+/// </summary>
+/// <param name="radian"></param>
+/// <returns></returns>
+Matrix3x3 MakeRotateMatrix(float radian);
+
+/// <summary>
+/// 平行移動行列を作成する
+/// </summary>
+/// <param name="translation"></param>
+/// <returns></returns>
+Matrix3x3 MakeTranslateMatrix(const Vector2& translation);
+
+/// <summary>
+/// アフィン変換行列を作成する
+/// </summary>
+/// <param name="scale"></param>
+/// <param name="radian"></param>
+/// <param name="translation"></param>
+/// <returns></returns>
+Matrix3x3 MakeAffineMatrix(const Vector2& scale, float radian, const Vector2& translation);
+
+/// <summary>
+/// 逆行列を作成する
+/// </summary>
+/// <param name="m"></param>
+/// <returns></returns>
+Matrix3x3 MakeInverseMatrix(const Matrix3x3& m);
+
+/// <summary>
+/// 平行投影行列を作成する
+/// </summary>
+/// <param name="left"></param>
+/// <param name="top"></param>
+/// <param name="right"></param>
+/// <param name="bottom"></param>
+/// <returns></returns>
+Matrix3x3 MakeOrthographicMatrix(float left, float top, float right, float bottom);
+
+/// <summary>
+/// ビューポート変換行列を作成する
+/// </summary>
+/// <param name="left"></param>
+/// <param name="top"></param>
+/// <param name="width"></param>
+/// <param name="height"></param>
+/// <returns></returns>
+Matrix3x3 MakeViewportMatrix(float left, float top, float width, float height);
