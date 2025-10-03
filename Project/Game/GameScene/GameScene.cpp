f931a5e13eb3ac133f9463f4e162d26c004eddce
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2025-10-03 20:47:10 +0900

    一応コミット

--- Project/Game/GameScene/GameScene.cpp
+++ Project/Game/GameScene/GameScene.cpp
@@ -9,26 +9,6 @@ void GameScene::Initialize(const MugenEngine* engine)
 	// 基底クラスの初期化
 	Scene::Initialize(engine);
 
-	// モデルを読み込む
-	modelHandle1_ = engine_->LoadModel("./Resources/Models/suzanneUV", "suzanneUV.obj");
-	modelHandle2_ = engine_->LoadModel("./Resources/Models/node", "node.gltf");
-
-	// モデルの生成と初期化
-	model1_ = std::make_unique<MeshModel>();
-	model1_->Initialize(engine_, camera3d_.get(), modelHandle1_);
-	model1_->worldTransform_->translation_ = Vector3(-5.0f, 0.0f, 30.0f);
-	model1_->material_->enableHalfLambert_ = true;
-	model1_->material_->enableSpecular_ = true;
-	model1_->material_->enableBlinnPhong_ = true;
-	model1_->material_->shininess_ = 30.0f;
-
-	model2_ = std::make_unique<MeshModel>();
-	model2_->Initialize(engine_, camera3d_.get(), modelHandle2_);
-	model2_->worldTransform_->translation_ = Vector3(5.0f, 0.0f, 30.0f);
-	model2_->material_->enableHalfLambert_ = true;
-	model2_->material_->enableSpecular_ = true;
-	model2_->material_->enableBlinnPhong_ = true;
-	model2_->material_->shininess_ = 30.0f;
 }
 
 /// <summary>
@@ -39,30 +19,6 @@ void GameScene::Update()
 	// 基底クラスの更新処理
 	Scene::Update();
 
-	model1_->worldTransform_->rotation_.x += 0.01f;
-	model1_->worldTransform_->rotation_.y += 0.01f;
-
-	model2_->worldTransform_->rotation_.z += 0.01f;
-	model2_->worldTransform_->rotation_.y += 0.01f;
-
-	timer_ += engine_->GetDeltaTime();
-
-	if (timer_ <= 1.0f)
-	{
-		model2_->SetModelHandle(modelHandle1_);
-	}
-	else if (timer_ <= 2.0f)
-	{
-		model2_->SetModelHandle(modelHandle2_);
-	}
-	else
-	{
-		timer_ = 0.0f;
-	}
-
-	// モデルの更新処理
-	model1_->Update();
-	model2_->Update();
 }
 
 /// <summary>
@@ -70,10 +26,6 @@ void GameScene::Update()
 /// </summary>
 void GameScene::Draw()
 {
-	// モデルの描画処理
-	model1_->Draw();
-	model2_->Draw();
-
 
 	// 基底クラスの描画処理
 	Scene::Draw();
--- Project/Game/GameScene/GameScene.h
+++ Project/Game/GameScene/GameScene.h
@@ -26,21 +26,6 @@ public:
 
 
 
-	// モデル2
-	std::unique_ptr<MeshModel> model2_ = nullptr;
-
-	// モデルハンドル2
-	uint32_t modelHandle2_ = 0;
-
-
-	// モデル1
-	std::unique_ptr<MeshModel> model1_ = nullptr;
-
-	// モデルハンドル1
-	uint32_t modelHandle1_ = 0;
-
-
-	// タイマー
-	float timer_ = 0.0f;
+	
 };
 
